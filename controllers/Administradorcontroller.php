a36a0c9333b8e76a3ae37097ba5eb76b2d50313e
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-06-14 21:46:50 -0500

    Nuevo cambio

--- controllers/Administradorcontroller.php
+++ controllers/Administradorcontroller.php
@@ -69,13 +69,41 @@ class AdministradorController
         require_once ('views/components/layout/footer.php');
     }
     public function create()
-    {
-        
+{
+    $servidor = "localhost";
+    $usuario = "root";
+    $password = "";
+    $bd = "base_proyecto";
+    $conexion = mysqli_connect($servidor, $usuario, $password, $bd);
 
-        require_once ('views/components/layout/head.php');
-        require_once ('views/administrador/create.php');
-        require_once ('views/components/layout/footer.php');
+    if (mysqli_connect_error()) {
+        die("Conexión fallida: " . mysqli_connect_error());
     }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Datos del formulario
+        $nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
+        $correo = mysqli_real_escape_string($conexion, $_POST['correo']);
+        $edad = (int) $_POST['edad'];
+
+        // Consulta para insertar un nuevo usuario
+        $query = "INSERT INTO usuarios (nombre, correo, edad) VALUES ('$nombre', '$correo', $edad)";
+
+        if (mysqli_query($conexion, $query)) {
+            echo "Nuevo usuario creado exitosamente";
+        } else {
+            echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+        }
+    }
+
+    require_once('views/components/layout/head.php');
+    require_once('views/administrador/index.php');
+    require_once('views/components/layout/footer.php');
+
+    // Cerrar la conexión
+    mysqli_close($conexion);
+}
+
 
     public function destroy()
     {
